29d9dac7f09be18916040c6e861d54e77d099131
Aamir251 <ecodeshub@gmail.com>
2024-07-07 11:25:19 +0530

    apostrpohe s fix

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -11,7 +11,7 @@ const AboutUsPage = () => {
         <p>At <strong>SD IT Solution</strong>, we specialize in providing essential software solutions tailored to meet the diverse needs of businesses and individuals alike. With a commitment to quality and customer satisfaction, we offer a comprehensive range of products including Microsoft Office, Windows operating systems, Office for Mac, and more.</p>
 
         <p>
-          Our mission is to simplify technology procurement by offering reliable software solutions that empower productivity and efficiency. Whether you're a small business looking to streamline operations or an individual seeking the latest software tools, SD IT Solution is your trusted partner.
+          Our mission is to simplify technology procurement by offering reliable software solutions that empower productivity and efficiency. Whether you&apos;re a small business looking to streamline operations or an individual seeking the latest software tools, SD IT Solution is your trusted partner.
         </p>
 
         <p>Backed by a team of experienced professionals, we ensure that every purchase is accompanied by excellent customer support and timely assistance. We believe in forging long-term relationships built on trust and reliability.</p>
