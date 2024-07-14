7bb5eb1f961ff3e33dc8dfbfc9e80d5c1987848d
Aamir251 <ecodeshub@gmail.com>
2024-07-14 11:19:30 +0530

    meta title in processing order page

--- app/processing-order/page.tsx
+++ app/processing-order/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { useSessionStorage } from "@/hooks/useSessionStorage";
-import { useEffect, useState } from "react";
-import { OrderDoc } from "@/sanity/lib/types";
-import { createOrderDocInSanity, mutateProductQuantityInSanity } from "@/utils/sanity";
-import ProcessingOrderLoader from "./_components/ProcessingOrderLoader";
-import { useRouter } from "next/navigation";
-import { Metadata } from "next";
+import { Metadata } from "next"
+import ProcessingOrder from "./_components/ProcessingOrder"
 
 type ProcessingOrderPageProps = {
   searchParams : {
@@ -20,52 +13,7 @@ export const metadata: Metadata = {
 
 const ProcessingOrderPage = ({ searchParams } : ProcessingOrderPageProps) => {
   
-  const router = useRouter()
-  const { orderId } = searchParams;
-  const { data, setData } = useSessionStorage("sd-order", {})
-  const { setData : setCartItems } = useSessionStorage("sd-cart", [])
-  
-  const [ isLoading, setIsLoading ] = useState<boolean>(true)
-
-  const isOrderDoc = (doc : OrderDoc | {}, orderId : string) : doc is OrderDoc => {
-    return "orderId" in doc && doc.orderId === orderId
-  }
-
-  useEffect(() => {
-
-    const createDocInSanity = async (doc : OrderDoc) => {
-      try {
-        console.log({ doc })
-        setIsLoading(true)
-        const { success, message  } = await createOrderDocInSanity(doc)
-
-        if (!success) throw new Error(message);
-
-        // handle success here
-        await mutateProductQuantityInSanity(doc.items)
-        
-      } catch (error) {
-        console.log({ error })
-      } finally {
-        setIsLoading(false)
-        router.push(`/order-success?email=${doc.email}&orderId=${doc.orderId}`)
-        setData({})
-        setCartItems([]) // clear the cart
-      }
-    }
-
-    if (data && isOrderDoc(data, orderId)) {
-      createDocInSanity(data)
-    }
-  },[data])
-
-
-  if (isLoading) {
-    return <ProcessingOrderLoader />
-  }
-  return (
-    <div>ProcessingOrderPage</div>
-  )
+  return <ProcessingOrder orderId={searchParams.orderId} />
 }
 
 export default ProcessingOrderPage
